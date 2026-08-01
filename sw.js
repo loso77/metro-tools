4310cce224b58d40c75a61a62e94536fbc86319c
loso77 <58019098+loso77@users.noreply.github.com>
2026-08-01 10:23:27 +0800

    发布 V2.8.4 到站时刻显示站名

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "metro-tools-v2.8.8-table-train-label";
+const CACHE_NAME = "metro-tools-v2.8.9-station-arrival-label";
 const APP_ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
   "./icon.svg",
   "./train-query/",
-  "./train-query/?v=2.8.3",
+  "./train-query/?v=2.8.4",
   "./train-query/index.html",
   "./train-query/manifest.json",
   "./train-query/sw.js",
